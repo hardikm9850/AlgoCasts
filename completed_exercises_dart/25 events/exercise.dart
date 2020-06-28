5c1c45f53c994ffb4ae30fbe75299e8a5ee32369
alfred schilken <alfred@schilken.de>
2020-06-28 17:11:59 +0200

    prepare exercise events

--- completed_exercises_dart/25 events/exercise.dart
+++ completed_exercises_dart/25 events/exercise.dart
@@ -1,27 +1,17 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
-// One string is an anagram of another if it uses the same characters
-// in the same quantity. Only consider characters, not spaces
-// or punctuation.  Consider capital letters to be the same as lower case
-// --- Examples
-//   anagrams('rail safety', 'fairy tales') --> True
-//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-//   anagrams('Hi there', 'Bye there') --> False
+// Create an 'eventing' library out of the
+// Events class.  The Events class should
+// have methods 'on', 'trigger', and 'off'.
 
-bool anagrams(String stringA, String stringB) {
-  print("A: ${sortCharsInString(filter(stringA).toUpperCase())}");
-  print("B: ${sortCharsInString(filter(stringB).toUpperCase())}");
-  return sortCharsInString(filter(stringA).toUpperCase()) ==
-      sortCharsInString(filter(stringB).toUpperCase());
-}
+class Events {
+  // Register an event handler
+  void on(String eventName, Function callback) {}
 
-RegExp regExp = new RegExp(r"\w");
+  // Trigger all callbacks associated
+  // with a given eventName
+  void trigger(String eventName) {}
 
-String filter(String input) {
-  return input.split('').where((char) => regExp.hasMatch(char)).join();
-}
-
-String sortCharsInString(String input) {
-  var charsList = input.split('')..sort();
-  return charsList.join();
+  // Remove all event handlers associated
+  // with the given eventName
+  void off(String eventName) {}
 }
--- completed_exercises_dart/25 events/exercise_test.dart
+++ completed_exercises_dart/25 events/exercise_test.dart
@@ -1,31 +1,88 @@
 import 'package:test/test.dart';
 import 'exercise.dart';
 
+class CallCounter {
+  int count = 0;
+
+  void call() {
+    count++;
+  }
+}
+
 void main() {
-  test('"hello" is an anagram of "llohe"', () {
-    expect(anagrams('hello', 'llohe'), true);
+  test('Events can be registered then triggered', () {
+    var events = new Events();
+
+    var cb1 = CallCounter();
+
+    events.on('click', cb1.call);
+    events.trigger('click');
+
+    expect(cb1.count, 1);
   });
 
-  test('"hello" is an anagram of "Hello"', () {
-    expect(anagrams('hello', 'Hello'), true);
+  test('Multiple events can be registered then triggered', () {
+    var events = new Events();
+
+    var cb1 = CallCounter();
+    var cb2 = CallCounter();
+
+    events.on('click', cb1.call);
+    events.on('click', cb2.call);
+    events.trigger('click');
+
+    expect(cb1.count, 1);
+    expect(cb2.count, 1);
   });
 
-  test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () {
-    expect(anagrams('Whoa! Hi!', 'Hi! Whoa!'), true);
+  test('Events can be triggered multiple times', () {
+    var events = new Events();
+
+    var cb1 = CallCounter();
+    var cb2 = CallCounter();
+
+    events.on('click', cb1.call);
+    events.trigger('click');
+    events.on('click', cb2.call);
+    events.trigger('click');
+    events.trigger('click');
+
+    expect(cb1.count, 3);
+    expect(cb2.count, 2);
   });
 
-  test('"One One" is not an anagram of "Two two two"', () {
-    expect(anagrams('One One', 'Two two two'), false);
+  test('Events can have different names', () {
+    var events = new Events();
+
+    var cb1 = CallCounter();
+    var cb2 = CallCounter();
+
+    events.on('click', cb1.call);
+    events.trigger('click');
+    events.on('hover', cb2.call);
+    events.trigger('click');
+    events.trigger('hover');
+
+    expect(cb1.count, 2);
+    expect(cb2.count, 1);
   });
 
-  test('"One one" is not an anagram of "One one c"', () {
-    expect(anagrams('One one', 'One one c'), false);
-  });
+  test('Events can be toggled off', () {
+    var events = new Events();
 
-  test(
-      '"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"',
-      () {
-    expect(
-        anagrams('A tree, a life, a bench', 'A tree, a fence, a yard'), false);
+    var cb1 = CallCounter();
+    var cb2 = CallCounter();
+
+    events.on('hover', cb2.call);
+
+    events.on('click', cb1.call);
+    events.trigger('click');
+    events.off('click');
+    events.trigger('click');
+
+    events.trigger('hover');
+
+    expect(cb1.count, 1);
+    expect(cb2.count, 1);
   });
 }
