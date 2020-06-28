78290c1058ea4fe56ef9a2ac833fd1f5b9d2d1d5
alfred schilken <alfred@schilken.de>
2020-06-28 14:44:35 +0200

    complete exercise binary search

--- completed_exercises_dart/23 bst/exercise.dart
+++ completed_exercises_dart/23 bst/exercise.dart
@@ -17,8 +17,54 @@ class Node {
   Node right;
   Node(this.data);
 
-  insert(int data) {}
+  _addLeaf(Node node, int data) {
+//    print("_addLeaf ${node.data} $data");
+    if (data < node.data) {
+      if (node.left != null) {
+//        print("down Leaf left");
+        _addLeaf(node.left, data);
+      } else {
+        node.left = Node(data);
+//        print("new Leaf left\n");
+        return;
+      }
+    } else {
+      if (node.right != null) {
+//        print("down Leaf right");
+        _addLeaf(node.right, data);
+      } else {
+        node.right = Node(data);
+//        print("new Leaf right\n");
+        return;
+      }
+    }
+    return;
+  }
+
+  Node _searchLeaf(Node node, int data) {
+//    print("_searchLeaf ${node.data}");
+    if (data == node.data) {
+      return node;
+    } else if (data < node.data) {
+      if (node.left != null) {
+        return _searchLeaf(node.left, data);
+      } else {
+        return null;
+      }
+    } else {
+      if (node.right != null) {
+        return _searchLeaf(node.right, data);
+      } else {
+        return null;
+      }
+    }
+  }
+
+  void insert(int data) {
+    _addLeaf(this, data);
+  }
+
   Node contains(int data) {
-    return null;
+    return _searchLeaf(this, data);
   }
 }
