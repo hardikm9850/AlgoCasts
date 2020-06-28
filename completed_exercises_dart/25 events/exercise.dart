1f7f6d707e211f0c694428348f4c3b952a541327
alfred schilken <alfred@schilken.de>
2020-06-28 17:22:56 +0200

    complete exercise events

--- completed_exercises_dart/25 events/exercise.dart
+++ completed_exercises_dart/25 events/exercise.dart
@@ -4,14 +4,32 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  Map<String, List<Function>> handlers = {};
+
   // Register an event handler
-  void on(String eventName, Function callback) {}
+  void on(String eventName, Function callback) {
+    if (handlers.containsKey(eventName)) {
+      handlers[eventName].add(callback);
+    } else {
+      handlers[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  void trigger(String eventName) {}
+  void trigger(String eventName) {
+    if (handlers.containsKey(eventName)) {
+      for (Function fn in handlers[eventName]) {
+        fn();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  void off(String eventName) {}
+  void off(String eventName) {
+    if (handlers.containsKey(eventName)) {
+      handlers.remove(eventName);
+    }
+  }
 }
