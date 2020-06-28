3881c94189675beba4a66150d27ace25ffb87cd7
alfred schilken <alfred@schilken.de>
2020-06-28 17:11:32 +0200

    exercise only partly solved

--- completed_exercises_dart/24 validate/exercise.dart
+++ completed_exercises_dart/24 validate/exercise.dart
@@ -8,6 +8,37 @@
 
 import 'node.dart';
 
+bool _validate(Node node, Function(Node, bool) fn) {
+//    print("_searchLeaf ${node.data}");
+  bool leftIsOk;
+  bool rightIsOk;
+
+  if (node.left != null) {
+    if (node.left.data < node.data) {
+      leftIsOk = validate(node.left);
+      fn(node, true);
+    } else {
+      leftIsOk = false;
+    }
+  } else {
+    leftIsOk = true;
+  }
+  if (node.right != null) {
+    if (node.right.data > node.data) {
+      rightIsOk = validate(node.right);
+      fn(node, false);
+    } else {
+      rightIsOk = false;
+    }
+  } else {
+    rightIsOk = true;
+  }
+  return leftIsOk && rightIsOk;
+}
+
 bool validate(Node node) {
-  return false;
+  bool simpleNodeChecksAreOk = _validate(node, (node, isLeft) {
+    print("${node.data} ${node.left?.data} ${node.right?.data}  $isLeft");
+  });
+  return simpleNodeChecksAreOk;
 }
--- completed_exercises_dart/24 validate/exercise_test.dart
+++ completed_exercises_dart/24 validate/exercise_test.dart
@@ -4,7 +4,7 @@ import 'node.dart';
 
 void main() {
   test('Validate recognizes a valid BST', () {
-    var n = new Node(10);
+    var n = Node(10);
     n.insert(5);
     n.insert(15);
     n.insert(0);
@@ -13,13 +13,47 @@ void main() {
     expect(validate(n), true);
   });
 
+//     10
+//    5  15
+//   0     20
+// 99
+  test('Validate recognizes an invalid BST', () {
+    var n = Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
+    n.left.left.left = Node(99);
+
+    expect(validate(n), false);
+  });
+
+//     10
+//    5  15
+//   0     20
+//    99
   test('Validate recognizes an invalid BST', () {
     var n = new Node(10);
     n.insert(5);
     n.insert(15);
     n.insert(0);
     n.insert(20);
-    n.left.left.right = new Node(999);
+    n.left.left.right = Node(99);
+
+    expect(validate(n), false);
+  });
+
+//     10
+//    5  15
+//   0     20
+//       12
+  test('Validate recognizes an invalid BST', () {
+    var n = new Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
+    n.right.right.left = Node(12);
 
     expect(validate(n), false);
   });
