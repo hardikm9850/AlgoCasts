b2f5038b1483931b9ff0db3ad7f22df31b028fdc
alfred schilken <alfred@schilken.de>
2020-06-29 15:20:38 +0200

    test with much bigger arrays and use stopwatch to measure the time

--- completed_exercises_dart/26 sorting/exercise_test.dart
+++ completed_exercises_dart/26 sorting/exercise_test.dart
@@ -2,36 +2,88 @@ import 'package:test/test.dart';
 import 'exercise.dart';
 
 void main() {
-  List<int> getArray() {
-    return [100, -40, 500, -124, 0, 21, 7];
+  List<int> createArray(int size) {
+    var generated = Iterable<int>.generate(size).toList();
+    generated.shuffle();
+    return generated;
   }
 
-  List<int> getSortedArray() {
-    return [-124, -40, 0, 7, 21, 100, 500];
+  List<int> sortArray(List<int> array) {
+    var sorted = [...array];
+    sorted.sort();
+    return sorted;
   }
+
+  int size = 1000;
+  List<int> randomArray = createArray(size);
+  List<int> sortedArray = sortArray(randomArray);
+
+  List<int> randomArrayDoubleSize = createArray(size * 10);
+  List<int> sortedArrayDoubleSize = sortArray(randomArrayDoubleSize);
 
   group('Bubble sort', () {
     test('sorts an array', () {
-      expect(bubbleSort(getArray()), getSortedArray());
+      var clonedArray = [...randomArray];
+      Stopwatch s = new Stopwatch();
+      s.start();
+      expect(bubbleSort(clonedArray), sortedArray);
+      print(
+          "bubbleSort size ${clonedArray.length} duration in microseconds ${s.elapsedMicroseconds}"); // 0
+    });
+
+    test('sorts an array bigger size', () {
+      var clonedArray = [...randomArrayDoubleSize];
+      Stopwatch s = new Stopwatch();
+      s.start();
+      expect(bubbleSort(clonedArray), sortedArrayDoubleSize);
+      print(
+          "bubbleSort size ${clonedArray.length} duration in microseconds ${s.elapsedMicroseconds}"); // 0
     });
   });
 
   group('Selection sort', () {
     test('sorts an array', () {
-      expect(selectionSort(getArray()), getSortedArray());
+      var clonedArray = [...randomArray];
+      Stopwatch s = new Stopwatch();
+      s.start();
+      expect(selectionSort(clonedArray), sortedArray);
+      print(
+          "selectionSort size ${clonedArray.length} duration in microseconds ${s.elapsedMicroseconds}"); // 0
+    });
+
+    test('sorts an array bigger size', () {
+      var clonedArray = [...randomArrayDoubleSize];
+      Stopwatch s = new Stopwatch();
+      s.start();
+      expect(selectionSort(clonedArray), sortedArrayDoubleSize);
+      print(
+          "selectionSort size ${clonedArray.length} duration in microseconds ${s.elapsedMicroseconds}"); // 0
     });
   });
 
   group('Merge sort', () {
     test('merge function can join together two sorted arrays', () {
       var left = [1, 10];
-      var right = [2, 8, 12];
-
-      expect(merge(left, right), [1, 2, 8, 10, 12]);
+      var right = [2, 8, 12, 15];
+      expect(merge(left, right), [1, 2, 8, 10, 12, 15]);
     });
 
     test('sorts an array', () {
-      expect(mergeSort(getArray()), getSortedArray());
+      var clonedArray = [...randomArray];
+      Stopwatch s = new Stopwatch();
+      s.start();
+      expect(mergeSort(clonedArray), sortedArray);
+      print(
+          "mergeSort size ${clonedArray.length} duration in microseconds ${s.elapsedMicroseconds}"); // 0
+    });
+
+    test('sorts an array bigger size', () {
+      var clonedArray = [...randomArrayDoubleSize];
+      Stopwatch s = new Stopwatch();
+      s.start();
+      expect(mergeSort(clonedArray), sortedArrayDoubleSize);
+      print(
+          "mergeSort size ${clonedArray.length}  duration in microseconds ${s.elapsedMicroseconds}"); // 0
     });
   });
 }
