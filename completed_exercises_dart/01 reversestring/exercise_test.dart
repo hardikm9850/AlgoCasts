c9cffa5247be6495eb9aa50ca6c86e09e0a106e0
alfred schilken <alfred@schilken.de>
2020-06-30 15:49:51 +0200

    delete not necessary equals in expect call

--- completed_exercises_dart/01 reversestring/exercise_test.dart
+++ completed_exercises_dart/01 reversestring/exercise_test.dart
@@ -3,27 +3,26 @@ import 'exercise.dart';
 
 void main() {
   test('Reverse reverses a string', () {
-    expect(reverse('abcd'), equals('dcba'));
+    expect(reverse('abcd'), 'dcba');
   });
 
   test('Reverse reverses a string', () {
-    expect(reverse('  abcd'), equals('dcba  '));
+    expect(reverse('  abcd'), 'dcba  ');
   });
 
   test('Reverse reverses2 a string', () {
-    expect(reverse2('abcd'), equals('dcba'));
+    expect(reverse2('abcd'), 'dcba');
   });
 
   test('Reverse reverses3 a string', () {
-    expect(reverse2('  abcd'), equals('dcba  '));
+    expect(reverse2('  abcd'), 'dcba  ');
   });
 
   test('Reverse reverses3 a string', () {
-    expect(reverse3('abcd'), equals('dcba'));
+    expect(reverse3('abcd'), 'dcba');
   });
 
   test('Reverse reverses3 a string', () {
-    expect(reverse3('  abcd'), equals('dcba  '));
+    expect(reverse3('  abcd'), 'dcba  ');
   });
-
 }
